28ee0d817336ac3bcd09bf764035a7b75ba96bb0
renanalves92 <166436253+renanalves92@users.noreply.github.com>
2024-10-24 20:36:43 -0300

    Add files via upload

---
+++ ListaPOOex02/ListaPOOEx2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Quadrado
+{
+    public double Aresta { get; set; }
+
+    public Quadrado(double aresta)
+    {
+        Aresta = aresta;
+    }
+
+    public double CalcularArea()
+    {
+        return Aresta * Aresta;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor da aresta do quadrado: ");
+        double aresta = Convert.ToDouble(Console.ReadLine());
+
+        Quadrado quadrado = new Quadrado(aresta);
+        Console.WriteLine($"A área do quadrado é: {quadrado.CalcularArea()}");
+    }
+}
