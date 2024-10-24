43638a29458d127ef29e460cdaccf3f4ac4f640f
renanalves92 <166436253+renanalves92@users.noreply.github.com>
2024-10-24 20:42:47 -0300

    Add files via upload

---
+++ ListaPOOex12/ListaPOOex12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Triangulo
+{
+    public double Lado1 { get; set; }
+    public double Lado2 { get; set; }
+    public double Lado3 { get; set; }
+
+    public Triangulo(double lado1, double lado2, double lado3)
+    {
+        Lado1 = lado1;
+        Lado2 = lado2;
+        Lado3 = lado3;
+    }
+
+    public bool VerificarSeFormaTriangulo()
+    {
+        return (Lado1 + Lado2 > Lado3) && (Lado1 + Lado3 > Lado2) && (Lado2 + Lado3 > Lado1);
+    }
+
+    public string ClassificarTriangulo()
+    {
+        if (Lado1 == Lado2 && Lado2 == Lado3)
+        {
+            return "Equilátero";
+        }
+        else if (Lado1 == Lado2 || Lado1 == Lado3 || Lado2 == Lado3)
+        {
+            return "Isósceles";
+        }
+        else
+        {
+            return "Escaleno";
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor do primeiro lado: ");
+        double lado1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o valor do segundo lado: ");
+        double lado2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o valor do terceiro lado: ");
+        double lado3 = Convert.ToDouble(Console.ReadLine());
+
+        Triangulo triangulo = new Triangulo(lado1, lado2, lado3);
+        if (triangulo.VerificarSeFormaTriangulo())
+        {
+            Console.WriteLine($"Os lados formam um triângulo {triangulo.ClassificarTriangulo()}.");
+        }
+        else
+        {
+            Console.WriteLine("Os valores não formam um triângulo.");
+        }
+    }
+}
---
+++ ListaPOOex13/ListaPOOex13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class TrianguloRetangulo
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+
+    public TrianguloRetangulo(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    public bool VerificarTrianguloRetangulo()
+    {
+        double hipotenusa = Math.Max(A, Math.Max(B, C));
+        double somaDosCatetos = A == hipotenusa ? B * B + C * C : (B == hipotenusa ? A * A + C * C : A * A + B * B);
+        return hipotenusa * hipotenusa == somaDosCatetos;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor do primeiro lado: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o valor do segundo lado: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o valor do terceiro lado: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        TrianguloRetangulo triangulo = new TrianguloRetangulo(a, b, c);
+        if (triangulo.VerificarTrianguloRetangulo())
+        {
+            Console.WriteLine("Os valores formam um triângulo retângulo.");
+        }
+        else
+        {
+            Console.WriteLine("Os valores não formam um triângulo retângulo.");
+        }
+    }
+}
