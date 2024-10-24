08a4aa8982d0c93fcf578db952172bb6870a7281
renanalves92 <166436253+renanalves92@users.noreply.github.com>
2024-10-24 20:36:11 -0300

    Add files via upload

---
+++ ListaPOOex01/ListaPOOex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Retangulo
+{
+    public double Base { get; set; }
+    public double Altura { get; set; }
+
+    public Retangulo(double baseRetangulo, double altura)
+    {
+        Base = baseRetangulo;
+        Altura = altura;
+    }
+
+    public double CalcularArea()
+    {
+        return Base * Altura;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a base do retângulo: ");
+        double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a altura do retângulo: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        Retangulo retangulo = new Retangulo(baseRetangulo, altura);
+        Console.WriteLine($"A área do retângulo é: {retangulo.CalcularArea()}");
+    }
+}
